9c41d8ccddb449c04ce5711f87996b57d7c27246
aeguribalabhadraraju <aeguriraju123@gmail.com>
2024-12-06 17:59:28 +0530

    This is my first commit

---
+++ src/Git/com/GitClass.java
@@ -0,0 +1,20 @@
+package Git.com;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+          
+		
+		String hungry= "yes";
+		
+		
+		if (hungry=="yes") {
+			
+			System.out.println("eat at 1'O clock something....");
+		}
+		else {
+			System.out.println("Do work......");
+		}
+
+	}
+}
