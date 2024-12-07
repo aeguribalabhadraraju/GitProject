016fdf8a0da61a75877985a7e32c1531d2c695b9
aeguribalabhadraraju <aeguriraju123@gmail.com>
2024-12-07 11:36:02 +0530

    commiting code with new changes

---
+++ src/Git/com/PrintNum.java
@@ -0,0 +1,14 @@
+package Git.com;
+
+public class PrintNum {
+
+	public static void main(String[] args) {
+         
+		
+		System.out.println(42);
+		for (int i=1; i<=10; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
